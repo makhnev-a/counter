4d88d47d2cfe8ac382eb87344aaa083dc8ed11d0
makhnev-a <ray.makhnev@gmail.com>
2020-05-23 21:01:34 +0300

    localStorage fix 2

--- src/App.js
+++ src/App.js
@@ -3,98 +3,119 @@ import './App.css';
 import Counter from "./Counter";
 import SetCounter from "./SetCounter";
 
-class App extends React.Component{
-  state = {
-    countNum: 0,
-    maxValue: 5,
-    minValue: 0,
-    error: null,
-    isDisabled: false,
-    setCounterToggle: false
-  };
+class App extends React.Component {
+    constructor(props) {
+      super(props);
 
-  componentDidMount() {
-    let counterData = JSON.parse(localStorage.getItem('counter'));
-    console.log(localStorage.getItem('counter'))
-    this.setState((prevState) => ({
-      ...prevState, counterData
-    }));
-  }
+      const counterData = JSON.parse(localStorage.getItem('counter') || '{}');
 
-  saveState = () => {
-    let stateAsString = JSON.stringify(this.state);
-    localStorage.setItem('counter', stateAsString)
-  };
+      let state = {
+          countNum: 0,
+          maxValue: 5,
+          minValue: 0,
+          error: null,
+          isDisabled: false,
+          setCounterToggle: false
+      };
 
-  onAddNum = () => {
-    if (this.state.countNum < this.state.maxValue) {
-      this.setState({
-        countNum: this.state.countNum + 1
-      });
+      if (Object.keys(counterData).length) {
+          state = counterData;
+      }
+
+      this.state = state;
     }
-  };
 
-  onClearNum = () => {
-    this.setState({
-      countNum: this.state.minValue
-    });
-  };
+    // state = {
+    //     countNum: 0,
+    //     maxValue: 5,
+    //     minValue: 0,
+    //     error: null,
+    //     isDisabled: false,
+    //     setCounterToggle: false
+    // };
 
-  setError = (error) => {
-    this.setState({error});
-  };
+    // componentDidMount() {
+    //     let counterData = JSON.parse(localStorage.getItem('counter') || '{}');
+    //     console.log(typeof counterData)
+    //     this.setState((prevState) => ({
+    //         ...prevState, ...counterData
+    //     }));
+    // }
 
-  setDisabled = (isDisabled) => this.setState({isDisabled});
+    saveState = () => {
+        let stateAsString = JSON.stringify(this.state);
+        localStorage.setItem('counter', stateAsString)
+    };
 
-  fixMaxValue = (maxVal) => {
-    this.setState({
-      maxValue: maxVal
-    });
-  };
+    onAddNum = () => {
+        if (this.state.countNum < this.state.maxValue) {
+            this.setState({
+                countNum: this.state.countNum + 1
+            });
+        }
+    };
 
-  fixStartValue = (startValue) => {
-    this.setState({
-      countNum: startValue,
-      minValue: startValue
-    });
-  };
+    onClearNum = () => {
+        this.setState({
+            countNum: this.state.minValue
+        });
+    };
 
-  changeSetToggle = () => {
-    this.setState({
-      setCounterToggle: !this.state.setCounterToggle
-    });
-  };
+    setError = (error) => {
+        this.setState({error});
+    };
 
-  render = () => {
-    return (
-        <div className="App">
-          <section className="App-header">
-            <SetCounter
-                className='test'
-                isDisabled={this.state.isDisabled}
-                setDisabled={this.setDisabled}
-                maxValue={this.state.maxValue}
-                startValue={this.state.countNum}
-                fixMaxValue={this.fixMaxValue}
-                fixStartValue={this.fixStartValue}
-                setError={this.setError}
-                setCounterToggle={this.state.setCounterToggle}
-                saveState={this.saveState}
-            />
-            <Counter
-                isDisabled={this.state.isDisabled}
-                messageError={this.state.error}
-                countNum={this.state.countNum}
-                maxValue={+this.state.maxValue}
-                minValue={this.state.minValue}
-                onAddNum={this.onAddNum}
-                onClearNum={this.onClearNum}
-                changeSetToggle={this.changeSetToggle}
-            />
-          </section>
-        </div>
-    );
-  };
+    setDisabled = (isDisabled) => this.setState({isDisabled});
+
+    fixMaxValue = (maxVal) => {
+        this.setState({
+            maxValue: maxVal
+        });
+    };
+
+    fixStartValue = (startValue) => {
+        this.setState({
+            countNum: startValue,
+            minValue: startValue
+        });
+    };
+
+    changeSetToggle = () => {
+        this.setState({
+            setCounterToggle: !this.state.setCounterToggle
+        });
+    };
+
+    render = () => {
+        return (
+            <div className="App">
+                <section className="App-header">
+                    <SetCounter
+                        className='test'
+                        isDisabled={this.state.isDisabled}
+                        setDisabled={this.setDisabled}
+                        maxValue={this.state.maxValue}
+                        startValue={this.state.countNum}
+                        fixMaxValue={this.fixMaxValue}
+                        fixStartValue={this.fixStartValue}
+                        setError={this.setError}
+                        setCounterToggle={this.state.setCounterToggle}
+                        saveState={this.saveState}
+                    />
+                    <Counter
+                        isDisabled={this.state.isDisabled}
+                        messageError={this.state.error}
+                        countNum={this.state.countNum}
+                        maxValue={+this.state.maxValue}
+                        minValue={this.state.minValue}
+                        onAddNum={this.onAddNum}
+                        onClearNum={this.onClearNum}
+                        changeSetToggle={this.changeSetToggle}
+                    />
+                </section>
+            </div>
+        );
+    };
 }
 
 export default App;
