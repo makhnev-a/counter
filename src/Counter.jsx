54958eb84e0e1baa73978e87cf050a18add13a9e
makhnev-a <ray.makhnev@gmail.com>
2020-06-14 22:38:35 +0300

    Counter to func component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,36 +2,34 @@ import React from "react";
 import Number from "./Number";
 import Button from "./Button";
 
-class Counter extends React.Component{
-    checkValueInc = () => (this.props.countNum === this.props.maxValue) || (this.props.countNum < 0);
-    checkValueRes = () => this.props.countNum < this.props.minValue + 1;
-    setBtnClick = () => this.props.changeSetToggle();
+const Counter = (props) => {
+    const checkValueInc = () => (props.countNum === props.maxValue) || (props.countNum < 0);
+    const checkValueRes = () => props.countNum < props.minValue + 1;
+    const setBtnClick = () => props.changeSetToggle();
 
-    render = () => {
-        return (
-            <div>
-                <Number
-                    countNum={this.props.countNum}
-                    maxValue={this.props.maxValue}
-                    messageError={this.props.messageError}
-                />
-                <Button
-                    title={'Inc'}
-                    disable={this.checkValueInc() ? true : this.props.isDisabled}
-                    handler={this.props.onAddNum}
-                />
-                <Button
-                    title={'Reset'}
-                    disable={this.checkValueRes() ? true : this.props.isDisabled}
-                    handler={this.props.onClearNum}
-                />
-                <Button
-                    title={'Set'}
-                    handler={this.setBtnClick}
-                />
-            </div>
-        );
-    };
+    return (
+        <div>
+            <Number
+                countNum={props.countNum}
+                maxValue={props.maxValue}
+                messageError={props.messageError}
+            />
+            <Button
+                title={'Inc'}
+                disable={checkValueInc() ? true : props.isDisabled}
+                handler={props.onAddNum}
+            />
+            <Button
+                title={'Reset'}
+                disable={checkValueRes() ? true : props.isDisabled}
+                handler={props.onClearNum}
+            />
+            <Button
+                title={'Set'}
+                handler={setBtnClick}
+            />
+        </div>
+    );
 }
 
 export default Counter;
